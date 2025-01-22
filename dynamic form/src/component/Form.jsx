85489d06306a8c1024da52453f77a43bc20b7257
krishnaParmar16 <kp497485@gmail.com>
2025-01-22 14:45:44 +0530

    dynamic form

--- dynamic form/src/component/Form.jsx
+++ dynamic form/src/component/Form.jsx
@@ -1,88 +1,206 @@
-import React, { useState } from 'react'
-import "../App.css"
+import React, { useState } from "react";
+import "../App.css";
 function Form() {
+  const [state, setState] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    skill: [""],
+    hobby:[""],
+  });
+  // const [skill,setskill]=useState([""])
 
-    const [state,setState]=useState({
-        firstName:"",
-        lastName:"",
-        email:"",
-        skill:[""]
-    })
-    // const [skill,setskill]=useState([""])
+  const addskill = () => {
+    setState({ ...state, skill: [...state.skill, ""] });
+  };
+  const addhobby = () => {
+    setState({ ...state, hobby: [...state.hobby, ""] });
+  };
 
-    const addskill=()=>{
-        setState({...state,skill:[...state.skill,""]})
-    }
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    // if (isNaN(name)) {
+      setState({ ...state, [name]: value });
+    // } else {
+    //   const arr = [...state.skill];
+    //   arr[name] = value;
+    //   setState({ ...state, skill: arr });
+    // }
+  };
 
-    const handleChange=(e)=>{
-        const {name,value}=e.target
-        if(isNaN(name))
-        {
-            setState({...state,[name]:value})
-        }
-        else{
-            const arr=[...state.skill]
-            arr[name]=value
-            setState({...state,skill:arr})
-        }
-    }
-    // console.log(state);
+  const handleSkill=(e,index)=>{
+      const arr=[...state.skill];
+      arr[index]=e.target.value;
+      setState({...state,skill:arr});
+  }
+
+  const handlehobby=(e,index)=>{
+    const arr=[...state.hobby];
+    arr[index]=e.target.value;
+    setState({...state,hobby:arr});
+}
+  // console.log(state);
+
+  const deleteSkill = (e) => {
+    const index = e.target.name;
+    console.log(index);
+    const arr=[...state.skill]
+    arr.splice(index,1);
+    setState({...state,skill:arr})
     
-    const deleteSkill=(e)=>{
-        const index=e.target.name
-        console.log(index);
-        // const arr=[...state.skill]
-        // arr.splice(index,1);
-        // setState({...state,skill:arr})
-        state.skill.splice(index,1)
-        setState({...state,skill:[...state.skill]})
-    }
+  };
+  const deletehobby = (e) => {
+    const index = e.target.name;
+    console.log(index);
+    const arr=[...state.hobby]
+    arr.splice(index,1);
+    setState({...state,hobby:arr})
+  };
 
-    const [arr,setarr]=useState([]);
-    const handleSubmit=(form)=>{
-        form.preventDefault()
-        setarr([...arr,state])
+  const [arr, setarr] = useState([]);
 
-        alert("Data added successfully")
+  const handleSubmit = (form) => {
+    form.preventDefault();
+    setarr([...arr, state]);
 
-        setState({
-            firstName: "",
-            lastName: "",
-            email: "",
-            skill: [""],
-        });
-    }
+    alert("Data added successfully");
 
-    console.log(arr);
-    
+    setState({
+      firstName: "",
+      lastName: "",
+      email: "",
+      skill: [""],
+      hobby:[""],
+    });
+  };
+
+  console.log(arr);
+
   return (
-    <div className='h-[auto] w-[35%]  ml-[32.5%] mt-[50px] drop-shadow-md shadow-xl '>
-        <h1 className='mt-[20px] text-[22px] text-center font-semibold'>Form</h1>
-         <form action="" className='flex flex-col items-center' onSubmit={handleSubmit}>
-            <label htmlFor="" >First Name</label>
-        <input type="text" name="firstName" id="" className=''  placeholder='Enter your first name' onChange={handleChange}  />
-   
-        <label htmlFor="">Last Name</label>
-        <input type="text" name="lastName" id=""  placeholder='Enter your last name' onChange={handleChange}  />
-    
-        <label htmlFor="">Email</label>
-        <input type="text" name="email" id="" placeholder='Enter your email' onChange={handleChange} />
-       
-    <label htmlFor="">Skills<input type="button" name="" className='h-[30px] bg-[black] text-[white] cursor-pointer text-[12px] ml-[20px] w-[95px]  ' id="" value='Add Skill ' onClick={addskill}/></label>
-    
-       {
-        state.skill.map((el,index)=>{
-            return <div className='flex mb-[0px] w-[70%]'>
-                 <input type="text" className='w-[75%]' name={index} id="" placeholder='Enter your skill' onChange={handleChange}  />
-                <input type="button" name={index} id="" value='Delete' onClick={deleteSkill}  className='h-[45px] w-[25%] cursor-pointer border text-[12px] bg-[red] text-[white]' />
-            </div>
-        })
-        }
+    <div className="h-[auto] w-[35%]  ml-[32.5%] mt-[50px] drop-shadow-md shadow-xl ">
+      <h1 className="mt-[20px] text-[22px] text-center font-semibold">Form</h1>
+      <form
+        action=""
+        className="flex flex-col items-center"
+        onSubmit={handleSubmit}
+      >
+        <label htmlFor="">First Name</label>
+        <input
+          type="text"
+          name="firstName"
+          id=""
+          className=""
+          value={state.firstName}
+          placeholder="Enter your first name"
+          onChange={handleChange}
+        />
 
-        <input type="submit" className='bg-[black] text-[white] mb-[20px] cursor-pointer hover:bg-[transparent] hover:text-[black] hover:border-[black] mt-[10px] delay-75' />
-    </form>
+        <label htmlFor="">Last Name</label>
+        <input
+          type="text"
+          name="lastName"
+          id=""
+          value={state.lastName}
+          placeholder="Enter your last name"
+          onChange={handleChange}
+        />
+
+        <label htmlFor="">Email</label>
+        <input
+          type="text"
+          name="email"
+          id=""
+          value={state.email}
+          placeholder="Enter your email"
+          onChange={handleChange}
+        />
+
+        <label htmlFor="">
+          Skills
+          <input
+            type="button"
+            name=""
+            className="h-[30px] bg-[black] text-[white] cursor-pointer text-[12px] ml-[20px] w-[95px]  "
+            id=""
+            value="Add Skill "
+            onClick={addskill}
+          />
+        </label>
+
+        {state.skill.map((el, index) => {
+          return (
+            <div className="flex mb-[0px] w-[70%]">
+              <input
+                type="text"
+                className="w-[75%]"
+                value={state.skill}
+                name={index}
+                id=""
+                placeholder="Enter your skill"
+                onChange={(e)=>handleSkill(e,index)}
+              />
+              {
+                state.skill.length>1?
+                <input
+                  type="button"
+                  name={index}
+                  id=""
+                  value="Delete"
+                  onClick={deleteSkill}
+                  className="h-[45px] w-[25%] cursor-pointer border text-[12px] bg-[red] text-[white]"
+                />:""
+              }
+            </div>
+          )
+        })}
+
+        <label htmlFor="">
+          Hobbies
+          <input
+            type="button"
+            name=""
+            className="h-[30px] bg-[black] text-[white] cursor-pointer text-[12px] ml-[20px] w-[95px]  "
+            id=""
+            value="Add Hobby "
+            onClick={addhobby}
+          />
+        </label>
+
+        {
+        state.hobby.map((el, index) => {
+          return (
+            <div className="flex mb-[0px] w-[70%]">
+              <input
+                type="text"
+                className="w-[75%]"
+                value={state.hobby}
+                name={index}
+                id=""
+                placeholder="Enter your hobby"
+                onChange={(e)=>handlehobby(e,index)}
+              />
+              {
+                state.hobby.length>1?
+                <input
+                  type="button"
+                  name={index}
+                  id=""
+                  value="Delete"
+                  onClick={deletehobby}
+                  className="h-[45px] w-[25%] cursor-pointer border text-[12px] bg-[red] text-[white]"
+                />:""
+              }
+            </div>
+          )
+        })}
+
+        <input
+          type="submit"
+          className="bg-[black] text-[white] mb-[20px] cursor-pointer hover:bg-[transparent] hover:text-[black] hover:border-[black] mt-[10px] delay-75"
+        />
+      </form>
     </div>
-  )
+  );
 }
 
-export default Form
+export default Form;
